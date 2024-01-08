e4c27d82b679511fd5a28209abd4e730c0ced090
Quang Dung Tran <dungtranquang97@gmail.com>
2024-01-08 10:32:33 +0100

    feat: update routing

--- src/frontend/convogaunlet-fe/src/App.tsx
+++ src/frontend/convogaunlet-fe/src/App.tsx
@@ -3,17 +3,18 @@ import MenuBar from "./components/MenuBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Live from "./pages/Live";
 import Gestures from "./pages/Gestures";
+import { Box } from "@mui/material";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div>
+      <Box component={"div"}>
         <MenuBar />
         <Routes>
           <Route path="/live-data" element={<Live />} />
           <Route path="/gestures" element={<Gestures />} />
         </Routes>
-      </div>
+      </Box>
     </Router>
   );
 };
