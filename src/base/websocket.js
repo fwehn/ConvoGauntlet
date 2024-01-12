b5bdcb729ec5b09f9d1fb4c4370cefc4c623bb98
fwehn <finn.wehn@web.de>
2024-01-12 18:17:51 +0100

    Added ESLint; Added Prettier; Added 'dummy' mode; Added documentation for websocket connection;

--- src/base/websocket.js
+++ src/base/websocket.js
@@ -1,21 +1,27 @@
-const {createServer} = require('node:http');
-const {Server} = require('socket.io');
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 
 const server = createServer();
-const io = new Server(server, {cors: {origin: "*"}});
+const io = new Server(server, { cors: { origin: "*" } });
 
 const wsPort = process.env.WSPORT || 3001;
 
-server.listen(wsPort, () => {
-	console.log(`Websocket connection available at ws://localhost:${wsPort}`);
-});
+function start() {
+    return new Promise((resolve, reject) => {
+        try {
+            server.listen(wsPort, (...params) => resolve([`Websocket: Opened at ws://localhost:${wsPort}`, ...params]));
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
 
 function emitGesture(gesture) {
-	io.emit('gesture', gesture);
+    io.emit("gesture", gesture);
 }
 
 function emitDebug(gesture) {
-	io.emit('debug', gesture);
+    io.emit("debug", gesture);
 }
 
-module.exports = {emitGesture, emitDebug};
+module.exports = { start, emitGesture, emitDebug };
